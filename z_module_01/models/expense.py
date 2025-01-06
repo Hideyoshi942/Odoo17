9debea63f8af3ecacf7630e4b54dc59c69c92562
Chu Tuấn Kiệt <96521130+Hideyoshi942@users.noreply.github.com>
2025-01-06 17:18:48 +0700

    Update view

--- z_module_01/models/expense.py
+++ z_module_01/models/expense.py
@@ -1,14 +1,22 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Expense(models.Model):
   _name = 'expense'
-  _description = 'Project check knowledge about odoo'
+  _inherit = ['mail.thread']
+  _description = 'Product Expense'
 
+
+  # trường hiển thị tên trên giao diện chi tiết về sản phẩm nếu không có sẽ sử dụng name thay thế
+  # _rec_name = 'reference'
+
+  reference = fields.Char(string="Name", required=True,
+                     default=lambda self: self.env['ir.sequence'].next_by_code(
+                       'expense.sequence'))
   name = fields.Char(string='Product_name', required=True)
   image = fields.Binary(string='Image', attachment=True)
   price = fields.Monetary(string="Price", currency_field="currency_id",
-                          required=True)
+                          required=True, tracking=True)
   currency_id = fields.Many2one('res.currency', string="Currency",
                                 required=True, default=lambda self: self.env.ref('base.USD'))
   quantity = fields.Integer(string='Quantity', required=True)
